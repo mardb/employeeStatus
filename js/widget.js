0a279d76bd151ca0eeee77bc17902a80a3f3475a
Marlene Guzman <marleneguzmanla@gmail.com>
2019-05-08 13:22:51 -0700

    adding meeting rooms widget to show room availability

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,27 @@
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function () {
+  if(roomRequest.readyState === 4 && roomRequest.status === 200) {
+    var rooms = JSON.parse(roomRequest.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
+//Repeat the same 4 steps of AJAX process
+//the URL for the JSON data is ../data/rooms.json
+//finished widget HTML 
+//complete HTML goes inside div with ID of 'roomList"
 $(document).ready(function () {
   $.getJSON('../data/employees.json', function (data) {
     var statusHTML = '<ul class="bulleted">';
